b2d506b528f08736358924e90a7d8dfae42463ac
yh2527 <110083173+yh2527@users.noreply.github.com>
2023-11-06 21:10:24 -0800

    rm original file

--- 607. Sales Person.sql
+++
@@ -1,9 +0,0 @@
-SELECT s.name
-FROM SalesPerson as s 
-WHERE NOT EXISTS (
-    SELECT 1
-    FROM Orders as o JOIN Company as c
-        ON o.com_id = c.com_id
-    WHERE s.sales_id = o.sales_id
-        AND c.name = 'RED'
-)
